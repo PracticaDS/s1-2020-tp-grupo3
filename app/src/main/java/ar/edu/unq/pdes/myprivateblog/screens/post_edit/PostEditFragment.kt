1ef8b8ada46395ee4fe3d7010ebfc500335a1416
Gaston Veliez <gastonveliez95@gmail.com>
2020-04-10 18:20:35 -0300

    Navegacion a fragment PostEdit y rellenado de datos en la vista de edicion

--- app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/post_edit/PostEditFragment.kt
+++ app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/post_edit/PostEditFragment.kt
@@ -2,26 +2,121 @@ package ar.edu.unq.pdes.myprivateblog.screens.post_edit
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import ar.edu.unq.pdes.myprivateblog.BaseFragment
+import ar.edu.unq.pdes.myprivateblog.ColorUtils
 import ar.edu.unq.pdes.myprivateblog.R
+import ar.edu.unq.pdes.myprivateblog.data.BlogEntry
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.fragment_post_edit.*
+import org.wordpress.aztec.Aztec
+import org.wordpress.aztec.ITextFormat
+import org.wordpress.aztec.glideloader.GlideImageLoader
+import org.wordpress.aztec.glideloader.GlideVideoThumbnailLoader
+import org.wordpress.aztec.toolbar.IAztecToolbarClickListener
+import timber.log.Timber
+import java.io.File
 
 class PostEditFragment : BaseFragment() {
     override val layoutId = R.layout.fragment_post_edit
 
     private val viewModel by viewModels<PostEditViewModel> { viewModelFactory }
 
+    private val args: PostEditFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // TODO!!!
+        viewModel.fetchBlogEntry(args.postId)
+
+        viewModel.post.observe(viewLifecycleOwner, Observer {
+            if (it != null) {
+                renderBlogEntry(it)
+            }
+        })
 
         btn_close.setOnClickListener {
             closeAndGoBack()
         }
 
+
+
+        viewModel.cardColor.observe(viewLifecycleOwner, Observer {
+            header_background.setBackgroundColor(it)
+            val itemsColor = ColorUtils.findTextColorGivenBackgroundColor(it)
+            title.setTextColor(itemsColor)
+            title.setHintTextColor(itemsColor)
+            btn_save.setColorFilter(itemsColor)
+            btn_close.setColorFilter(itemsColor)
+
+            applyStatusBarStyle(it)
+        })
+
+        title.doOnTextChanged { text, start, count, after ->
+            viewModel.titleText.postValue(text.toString())
+        }
+
+        body.doOnTextChanged { text, start, count, after ->
+            viewModel.bodyText.value = body.toFormattedHtml()
+            Timber.d(viewModel.bodyText.value)
+        }
+
+        btn_save.setOnClickListener {
+            viewModel.updatePost()
+        }
+
+        btn_close.setOnClickListener {
+            closeAndGoBack()
+        }
+
+        color_picker.onColorSelectionListener = {
+            viewModel.cardColor.postValue(it)
+        }
+
+        context?.apply {
+            Aztec.with(body, source, formatting_toolbar, object : IAztecToolbarClickListener {
+                override fun onToolbarCollapseButtonClicked() {
+                }
+
+                override fun onToolbarExpandButtonClicked() {
+                }
+
+                override fun onToolbarFormatButtonClicked(
+                    format: ITextFormat,
+                    isKeyboardShortcut: Boolean
+                ) {
+                }
+
+                override fun onToolbarHeadingButtonClicked() {
+                }
+
+                override fun onToolbarHtmlButtonClicked() {
+                }
+
+                override fun onToolbarListButtonClicked() {
+                }
+
+                override fun onToolbarMediaButtonClicked(): Boolean = false
+
+            })
+                .setImageGetter(GlideImageLoader(this))
+                .setVideoThumbnailGetter(GlideVideoThumbnailLoader(this))
+        }
+
+    }
+
+    fun renderBlogEntry(post: BlogEntry) {
+        title.setText(post.title)
+
+        header_background.setBackgroundColor(post.cardColor)
+        applyStatusBarStyle(post.cardColor)
+        title.setTextColor(ColorUtils.findTextColorGivenBackgroundColor(post.cardColor))
+        if (post.bodyPath != null && context != null) {
+            body.fromHtml(File(context?.filesDir, post.bodyPath).readText())
+        }
     }
 
     private fun closeAndGoBack() {
