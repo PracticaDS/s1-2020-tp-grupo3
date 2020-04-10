1df9c409cd56de491af26463c7c4d22160f8f188
Gaston Veliez <gastonveliez95@gmail.com>
2020-04-10 18:20:35 -0300

    Navegacion a fragment PostEdit y rellenado de datos en la vista de edicion

--- app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/post_edit/PostEditViewModel.kt
+++ app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/post_edit/PostEditViewModel.kt
@@ -1,8 +1,13 @@
 package ar.edu.unq.pdes.myprivateblog.screens.post_edit
 
 import android.content.Context
+import android.graphics.Color
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ar.edu.unq.pdes.myprivateblog.data.BlogEntriesRepository
+import ar.edu.unq.pdes.myprivateblog.data.BlogEntry
+import ar.edu.unq.pdes.myprivateblog.data.EntityID
+import ar.edu.unq.pdes.myprivateblog.rx.RxSchedulers
 import javax.inject.Inject
 
 
@@ -10,7 +15,29 @@ class PostEditViewModel @Inject constructor(
     val blogEntriesRepository: BlogEntriesRepository,
     val context: Context
 ) : ViewModel() {
+    enum class State {
+        EDITING, SUCCESS, ERROR
+    }
 
-    //TODO!!!
+    val state = MutableLiveData(State.EDITING)
+    val titleText = MutableLiveData("")
+    val bodyText = MutableLiveData("")
+    val cardColor = MutableLiveData<Int>(Color.LTGRAY)
 
+    var post = MutableLiveData<BlogEntry?>()
+
+    fun fetchBlogEntry(id: EntityID) {
+
+        val disposable = blogEntriesRepository
+            .fetchById(id)
+            .compose(RxSchedulers.flowableAsync())
+            .subscribe {
+                post.value = it
+            }
+    }
+
+    fun updatePost() {
+        //TODO
+
+    }
 }
