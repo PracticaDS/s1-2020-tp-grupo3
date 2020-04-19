7538124afb32d960d9493829d9ed2420f26db479
Gaston Veliez <gastonveliez95@gmail.com>
2020-04-19 16:21:58 -0300

    Agregada dependencia espresso.web y test del body al editar

--- app/src/androidTest/java/ar/edu/unq/pdes/myprivateblog/PostEditTest.kt
+++ app/src/androidTest/java/ar/edu/unq/pdes/myprivateblog/PostEditTest.kt
@@ -10,20 +10,22 @@ import androidx.test.espresso.Root
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.BoundedMatcher
-import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.web.assertion.WebViewAssertions.webMatches
+import androidx.test.espresso.web.sugar.Web.onWebView
+import androidx.test.espresso.web.webdriver.DriverAtoms.findElement
+import androidx.test.espresso.web.webdriver.DriverAtoms.getText
+import androidx.test.espresso.web.webdriver.Locator
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import org.hamcrest.Description
 import org.hamcrest.Matcher
-import org.hamcrest.Matchers.not
+import org.hamcrest.Matchers.containsString
 import org.hamcrest.TypeSafeMatcher
-import org.hamcrest.core.Is.`is`
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import kotlin.coroutines.coroutineContext
 
 
 @RunWith(AndroidJUnit4::class)
@@ -34,6 +36,10 @@ class PostEditTest {
     var activityRule: ActivityScenarioRule<MainActivity> =
         ActivityScenarioRule(MainActivity::class.java)
 
+    val NEW_TITLE = "Nuevo Titulo"
+    val OLD_TITLE = "Nuevo post"
+    val NEW_BODY = "Este es un post de prueba editado"
+    val OLD_BODY = "Este es un post de prueba"
 
     @Test
     fun whenTappingOnUpdatePost_postEditionScreenShouldOpen() {
@@ -44,16 +50,16 @@ class PostEditTest {
         onView(withId(R.id.title)).perform(clearText())
 
         onView(withId(R.id.title))
-            .perform(click(),replaceText("Nuevo post"))
+            .perform(click(),replaceText(OLD_TITLE))
 
         onView(withId(R.id.body))
-            .perform(click(),replaceText("Este es un post de prueba"))
+            .perform(click(),replaceText(OLD_BODY))
 
         onView(withId(R.id.btn_save)).perform(click())
 
         onView(withId(R.id.btn_edit)).perform(click())
 
-        onView(withId(R.id.title)).check(matches(withText("Nuevo post")))
+        onView(withId(R.id.title)).check(matches(withText(OLD_TITLE)))
 
         val colorPicked = Color.LTGRAY;
         onView(withId(R.id.header_background)).check(matches(withBackgroundColor(colorPicked)))
@@ -65,25 +71,26 @@ class PostEditTest {
 
     @Test
     fun whenEditingPost_postFieldsInPostDetailsViewShouldHaveChanged() {
-
         onView(withId(R.id.create_new_post))
             .perform(click())
 
         onView(withId(R.id.title)).perform(clearText())
 
         onView(withId(R.id.title))
-            .perform(click(),replaceText("Nuevo post"))
+            .perform(click(),replaceText(OLD_TITLE))
 
         onView(withId(R.id.body))
-            .perform(click(),replaceText("Este es un post de prueba"))
+            .perform(click(),replaceText(OLD_BODY))
 
         onView(withId(R.id.btn_save)).perform(click())
 
         onView(withId(R.id.btn_edit)).perform(click())
 
         onView(withId(R.id.title)).perform(click(),
-            replaceText("Nuevo Titulo")
+            replaceText(NEW_TITLE)
         )
+
+        onView(withId(R.id.body)).perform(click(), replaceText(NEW_BODY))
 
         val colorToPick = Color.parseColor("#b39ddb")
 
@@ -91,9 +98,15 @@ class PostEditTest {
 
         onView(withId(R.id.btn_save)).perform(click())
 
-        onView(withId(R.id.title)).check(matches(withText("Nuevo Titulo")))
+        onView(withId(R.id.title)).check(matches(withText(NEW_TITLE)))
 
         onView(withId(R.id.header_background)).check(matches(withBackgroundColor(colorToPick)))
+
+
+        //Como escribo un texto simple en el editor puedo chequear el texto de esta manera
+        onWebView(withId(R.id.body)).forceJavascriptEnabled()
+            .withElement(findElement(Locator.TAG_NAME,"body")).check(webMatches(getText(),
+                containsString(NEW_BODY)))
     }
 
 
@@ -106,10 +119,10 @@ class PostEditTest {
         onView(withId(R.id.title)).perform(clearText())
 
         onView(withId(R.id.title))
-            .perform(click(),replaceText("Nuevo post"))
+            .perform(click(),replaceText(OLD_TITLE))
 
         onView(withId(R.id.body))
-            .perform(click(),replaceText("Este es un post de prueba"))
+            .perform(click(),replaceText(OLD_BODY))
 
         onView(withId(R.id.btn_save)).perform(click())
 
